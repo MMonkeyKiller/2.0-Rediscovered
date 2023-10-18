8a6f2e76a100f4767b98c8b222e2a49b217da651
MMonkeyKiller <37459591+MMonkeyKiller@users.noreply.github.com>
2023-10-18 01:03:43 -0300

    Replace `@Redirect` with `@ModifyArgs` on `LivingEntityRendererMixin.java`

--- src/main/java/me/monkeykiller/v2_0_rediscovered/client/chickens/mixin/LivingEntityRendererMixin.java
+++ src/main/java/me/monkeykiller/v2_0_rediscovered/client/chickens/mixin/LivingEntityRendererMixin.java
@@ -1,21 +1,22 @@
 package me.monkeykiller.v2_0_rediscovered.client.chickens.mixin;
 
 import me.monkeykiller.v2_0_rediscovered.common.chickens.DiamondChickenAccessor;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.client.render.entity.model.EntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.passive.ChickenEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(LivingEntityRenderer.class)
 public abstract class LivingEntityRendererMixin {
+    @Unique
     private boolean replace;
 
     @Inject(at = @At("HEAD"), method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V")
@@ -24,13 +25,12 @@ public abstract class LivingEntityRendererMixin {
             replace = ((DiamondChickenAccessor) chicken).isDiamondChicken();
     }
 
-    @Redirect(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"))
-    public void diamondChickenColor(EntityModel<?> instance, MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+    @ModifyArgs(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"))
+    public void diamondChickenColor(Args args) {
         if (replace) {
-            red = 0.5f;
-            green = 0.5f;
-            blue = 1f;
+            args.set(5, 0.5f); // red
+            args.set(6, 0.5f); // green
+            args.set(7, 1f);   // blue
         }
-        instance.render(matrixStack, vertexConsumer, light, overlay, red, green, blue, alpha);
     }
 }
